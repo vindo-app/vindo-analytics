fd416ef53b116b75ee3ea6beaedf52407d4a4b89
tbodt <tblodt@icloud.com>
2015-11-22 21:44:15 +0000

    store ip addresses

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -1,6 +1,6 @@
 class AnalyticsController < ApplicationController
     def ping
-        uuid = params[:uuid]
+        uuid = request.remote_ip
         version = params[:version]
         
         @ping = Ping.find_by_uuid(uuid) || Ping.create(uuid: uuid)
