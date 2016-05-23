9e7b5f1ee9e4694eb536b52202823e58f639b689
Theodore Dubois <tblodt@icloud.com>
2016-05-23 14:03:09 -0700

    add hashify

--- app/models/ping.rb
+++ app/models/ping.rb
@@ -1,3 +1,10 @@
 class Ping < ActiveRecord::Base
     validates :uuid, uniqueness: true, presence: true
+
+    def hashify
+        {
+            "uuid" => uuid,
+            "version" => version
+        }
+    end
 end
