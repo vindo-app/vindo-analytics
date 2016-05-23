095df8861a7d0e9a2f79de7f106f35b478c11159
Theodore Dubois <tblodt@icloud.com>
2016-05-23 15:33:20 -0700

    adjust configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,11 +20,7 @@ Rails.application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
-
-  # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
+  config.serve_static_files = false
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
