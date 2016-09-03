fc59ce2b64521125978a832f2de6107a907d310b
Theodore Dubois <tblodt@icloud.com>
2016-09-02 19:31:17 -0700

    scaffold pings display

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+    resources :pings, only: [:index, :show]
+
     get "/ping" => "analytics#ping"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
