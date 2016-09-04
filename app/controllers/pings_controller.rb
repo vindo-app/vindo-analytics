c7496ce4cdc373d10bf5cbdae0f7aab9315fe3bb
Theodore Dubois <tblodt@icloud.com>
2016-09-04 16:31:16 -0700

    require authentication for the UI

--- app/controllers/pings_controller.rb
+++ app/controllers/pings_controller.rb
@@ -1,58 +1,19 @@
 class PingsController < ApplicationController
-  before_action :set_ping, only: [:show, :edit, :update, :destroy]
+    http_basic_authenticate_with name: ENV["UI_USERNAME"], password: ENV["UI_PASSWORD"]
+    before_action :set_ping, only: [:show, :edit, :update, :destroy]
 
-  # GET /pings
-  def index
-    @pings = Ping.all
-  end
-
-  # GET /pings/1
-  def show
-  end
-
-  # GET /pings/new
-  def new
-    @ping = Ping.new
-  end
-
-  # GET /pings/1/edit
-  def edit
-  end
-
-  # POST /pings
-  def create
-    @ping = Ping.new(ping_params)
-
-    if @ping.save
-      redirect_to @ping, notice: 'Ping was successfully created.'
-    else
-      render :new
+    # GET /pings
+    def index
+        @pings = Ping.all
     end
-  end
 
-  # PATCH/PUT /pings/1
-  def update
-    if @ping.update(ping_params)
-      redirect_to @ping, notice: 'Ping was successfully updated.'
-    else
-      render :edit
+    # GET /pings/1
+    def show
     end
-  end
 
-  # DELETE /pings/1
-  def destroy
-    @ping.destroy
-    redirect_to pings_url, notice: 'Ping was successfully destroyed.'
-  end
-
-  private
+    private
     # Use callbacks to share common setup or constraints between actions.
     def set_ping
-      @ping = Ping.find(params[:id])
-    end
-
-    # Only allow a trusted parameter "white list" through.
-    def ping_params
-      params.require(:ping).permit(:uuid, :version, :ip)
+        @ping = Ping.find(params[:id])
     end
 end
